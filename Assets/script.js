c671117448db58b89252a13fadb36987c3826965
Servbt <arianrezvani00@gmail.com>
2022-07-04 16:57:33 -0400

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,28 @@
+
+
+
+// Work Day Scheduler
+
+
+// functionality for saving notes //
+// listen for a save button
+// grab whatever was typed in the text area
+// then save it to local storage
+
+// functionality for geting previous values //
+// get the value from local storage
+// and then give the textarea the same value created from local storage
+
+// functionality for time row colors //
+// compare the current time with time displayed in each block
+// do this with moment.js
+// set up variables equal to the time of day using moment.js
+// loop over the blocks
+// compare block time to current time
+
+// if time is less than or equal too current time then we change color to gray
+// else if  current time is same change bg color to red
+// else turn the future rows to green
+
+
+// use moment to grab day and display it
